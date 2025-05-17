68c160d07e8d239f806a79ac7179e488e8ab74a5
Lgow0 <tejhomuhammad@gmail.com>
2025-05-18 01:27:13 +0700

    feat: opening(50%)

--- components/open-invite/index.js
+++ components/open-invite/index.js
@@ -1,28 +1,18 @@
-import Home from "@/pages/index";
-import { useState } from "react";
+import React from 'react';
 
-const OpenInvite = () => {
-    const [isValid, setIsValid] = useState(false);
-
-    const handleClick = () => {
-        const valid = true;
-        if (valid) {
-            setIsValid(true);
-        } else {
-            alert("Invalid access");
-        }
-    }
-
+const OpenInvite = ({onValidate}) => {
+     const handleClick = () => {
+        onValidate();
+    };
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold mb-4">Open Invite</h1>
-        <p className="text-lg mb-4">Name Guest</p>
-        <button 
-        onClick={handleClick}
-        className="bg-dark-500 text-white px-4 py-2 rounded">
-            Open Invite
-        </button>
-        {isValid && <Home/>}
+            <h1 className="text-2xl font-bold mb-4">Open Invite</h1>
+                <p className="text-lg mb-4">Name Guest</p>
+                <button 
+                onClick={handleClick}
+                className="bg-dark-500 text-white px-4 py-2 rounded">
+                    Open Invite
+                </button>
         </div>
     );
 }
