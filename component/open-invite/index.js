295360ab00a05c1ccf5d6b1d162c3067d0107902
Lgow0 <tejhomuhammad@gmail.com>
2025-05-21 15:37:22 +0700

    fix: update guest name display to use pathname

--- component/open-invite/index.js
+++ component/open-invite/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { Fade } from 'react-reveal'
+import {usePathname} from 'next/navigation'
+import { useRouter } from 'next/router'
 
 const OpenInvite = ({onValidate}) => {
      const handleClick = () => {
         onValidate()
+        router.push('/')
     }
+    const router = useRouter()
+    const pathname = usePathname()
+    
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <Fade bottom duration={1000}> 
             <h1 className="text-2xl font-bold mb-4">Open Invite</h1>
-                <p className="text-lg mb-4">Name Guest</p>
+                <p className="text-lg mb-4">{pathname.replace('/', '')} & Partner</p>
                 <Button 
                  onClick={handleClick}
                  className="bg-dark-500 text-white font-font-semibold px-4 py-2 rounded-xl">
